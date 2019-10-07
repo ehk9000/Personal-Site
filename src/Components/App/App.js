69154b68e089e020b07fa5a2d2db16ff26a65c9e
Erik Konner <erikkonner@gmail.com>
2019-10-07 14:26:10 -0600

    Imported icons, footer styles, footer component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Controls from '../Controls/Controls';
 import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="App">
        <NavBar />
        <Controls /> 
+       <Footer />
     </div>
   );
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <footer>
       
+
     </footer>
   );
 }
