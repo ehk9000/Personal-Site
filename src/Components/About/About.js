5830f80fe61f9f9a4155785c70801697a6d6b623
Erik Konner <erikkonner@gmail.com>
2019-12-17 14:27:55 -0700

    Adjusted padding on bio

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,23 +3,25 @@ import ekPic from '../../assets/images/Ek-Profile.jpg';
 
 const About = () => {
   return (
-    <section className="about-wrapper">
-      <img src={ekPic} alt="Erik Konner Profile" className="prof-pic"/>
-      <article className="about-bio">
-        <p>Hi! My name is Erik Konner, and I'm a Finance, and Customer Service Associate
-          turned Front End Software Engineer. My strengths include React, Redux, problem-solving,
-          and communication. I'm an empathetic developer who thrives in teams and loves learning 
-          new skills.</p>
-        <p>At Colorado College, I studied Political Science and Economics, and after graduating,
-          I found employment at Oppenheimer Funds. There, after receiving my Series 6 financial 
-          license, I worked in their inbound call center, performing trades, and was a resource 
-          to educate clients on their investments. Life in that job presented new puzzles every 
-          day, and I found great satisfaction in solving these new problems.</p>
-        <p>After a full year working there, I yearned for new skills - something to set me
-           apart and that encompassed the challenge and freshness of my old job. That is 
-           what led me to Turing and all the excitement of joining a new industry. </p>
-      </article>
-    </section>
+    <div >
+      <section className="about-wrapper">
+        <img src={ekPic} alt="Erik Konner Profile" className="prof-pic"/>
+        <article className="about-bio">
+          <p>Hi! My name is Erik Konner, and I'm a Finance, and Customer Service Associate
+            turned Front End Software Engineer. My strengths include React, Redux, problem-solving,
+            and communication. I'm an empathetic developer who thrives in teams and loves learning 
+            new skills.</p>
+          <p>At Colorado College, I studied Political Science and Economics, and after graduating,
+            I found employment at Oppenheimer Funds. There, after receiving my Series 6 financial 
+            license, I worked in their inbound call center, performing trades, and was a resource 
+            to educate clients on their investments. Life in that job presented new puzzles every 
+            day, and I found great satisfaction in solving these new problems.</p>
+          <p>After a full year working there, I yearned for new skills - something to set me
+            apart and that encompassed the challenge and freshness of my old job. That is 
+            what led me to Turing and all the excitement of joining a new industry. </p>
+        </article>
+      </section>
+    </div>
   )
 }
 
