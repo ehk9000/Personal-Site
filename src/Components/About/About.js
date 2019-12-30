fe27a1817b179909a3743cc9be5750b358d1c0c0
Erik Konner <erikkonner@gmail.com>
2019-12-30 14:36:40 -0700

    Changed brs to p tags, updated margins

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,15 +10,13 @@ const About = () => {
           <p>Hi! My name is Erik Konner, and I'm a Finance, and Customer Service Associate
             turned Front End Software Engineer. My strengths include React, Redux, problem-solving,
             and communication. I'm an empathetic developer who thrives in teams and loves learning 
-            new skills.
-            <br />
-            At Colorado College, I studied Political Science and Economics, and after graduating,
+            new skills.</p>
+            <p> At Colorado College, I studied Political Science and Economics, and after graduating,
             I found employment at Oppenheimer Funds. There, after receiving my Series 6 financial 
             license, I worked in their inbound call center, performing trades, and was a resource 
             to educate clients on their investments. Life in that job presented new puzzles every 
-            day, and I found great satisfaction in solving these new problems.
-            <br />
-            After a full year working there, I yearned for new skills - something to set me
+            day, and I found great satisfaction in solving these new problems. </p>
+            <p>After a full year working there, I yearned for new skills - something to set me
             apart and that encompassed the challenge and freshness of my old job. That is 
             what led me to Turing and all the excitement of joining a new industry. </p>
         </article>
