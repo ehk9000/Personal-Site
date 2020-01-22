6b9adf4fed3741164258cf80a3290a83829881e9
Erik Konner <erikkonner@gmail.com>
2020-01-21 17:20:40 -0700

    Importing screenshots of projects

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -5,6 +5,8 @@ const Portfolio = () => {
       <section>
         <h2>My Portfolio</h2>
         <article>
+          <img src={require("../../assets/images/CoderDojo.png")} alt="Splash Page for CoderDojo Website"/>
+          <img src={require("../../assets/images/TrapperKeeper.png")} alt="Splash Page for TrapperKeeper"/>
 
 
         </article>
