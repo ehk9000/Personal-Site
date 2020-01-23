8809f4522fafb9aeadeac92f48880df209e10300
Erik Konner <erikkonner@gmail.com>
2020-01-23 13:48:22 -0700

    Sizing of images

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 const Portfolio = () => {
     return (
-      <section>
+      <main>
         <h2>My Portfolio</h2>
-        <article>
-          <img src={require("../../assets/images/CoderDojo.png")} alt="Splash Page for CoderDojo Website"/>
-          <img src={require("../../assets/images/TrapperKeeper.png")} alt="Splash Page for TrapperKeeper"/>
-
-
-        </article>
+        <section className="portfolio-container">
+          <article>
+            <img src={require("../../assets/images/CoderDojo.png")} alt="Splash Page for CoderDojo Website"/>
+          </article>
+          <article>
+            <img src={require("../../assets/images/TrapperKeeper.png")} alt="Splash Page for TrapperKeeper"/>
+          </article>
+        </section>
   
-      </section>
+      </main>
     )
 }
 
